71abb43af6f31cdac67dff66610f2beca0323985
mohsin094 <mohsinkhanzda@gmail.com>
2021-11-10 18:25:07 +0500

    First commit

---
+++ resources/views/myTestMail.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ItsolutionStuff.com</title>
+</head>
+<body>
+    <h1>Hi</h1>
+    <a href="{{$details['Link']}}">Click me!</a>
+
+
+    <p>Thank you</p>
+</body>
+</html>
